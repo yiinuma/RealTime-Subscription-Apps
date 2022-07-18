708d3552ddfc730a604c6a189306fa83e559398e
yiinuma <y.iinuma211@gmail.com>
2022-07-18 18:02:04 +0900

    postform レイアウト調整

--- src/components/specificPage/PostForm/index.tsx
+++ src/components/specificPage/PostForm/index.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, FC, memo } from 'react'
 import Image from 'next/image'
-import { Input } from '@mantine/core'
+import { Button, Input } from '@mantine/core'
 import { AiFillCamera } from 'react-icons/ai'
 
 import useStore from 'store'
@@ -53,49 +53,45 @@ export const PostFormMemo: FC = () => {
           update({ ...editedPost, description: e.target.value })
         }
       />
+      <div className='flex items-center justify-center'>
+        <div className='flex justify-center'>
+          <label htmlFor='post'>
+            <div className='flex cursor-pointer items-center justify-center'>
+              {postUrl ? (
+                <Image
+                  src={postUrl}
+                  alt='Image'
+                  className='rounded'
+                  width={320}
+                  height={280}
+                  objectFit='contain'
+                />
+              ) : (
+                <>
+                  <AiFillCamera className='h-10 w-10 text-blue-500' />
+                  <p className='ml-4'>select image</p>
+                </>
+              )}
+            </div>
+          </label>
 
-      <div className='flex justify-center'>
-        {postUrl ? (
-          <Image
-            src={postUrl}
-            alt='Image'
-            className='rounded'
-            width={150}
-            height={150}
-            objectFit='contain'
+          <input
+            className='hidden'
+            type='file'
+            id='post'
+            accept='image/*'
+            onChange={async (e) => {
+              await useMutateUploadPostImg.mutateAsync(e)
+              e.target.value = ''
+            }}
           />
-        ) : (
-          <p>added image</p>
-        )}
+        </div>
       </div>
 
-      <div className='flex justify-center'>
-        <label htmlFor='post'>
-          <AiFillCamera className='mt-2 h-7 w-7 cursor-pointer text-gray-500' />
-        </label>
-        <input
-          className='hidden'
-          type='file'
-          id='post'
-          accept='image/*'
-          onChange={async (e) => {
-            await useMutateUploadPostImg.mutateAsync(e)
-            e.target.value = ''
-          }}
-        />
-      </div>
-
-      <div className='my-3 flex justify-center'>
-        <button
-          data-testid='btn-post'
-          type='submit'
-          className={`rounded ${
-            useMutateUploadPostImg.isLoading || !editedPost.title ? 'bg-gray-300' : 'bg-indigo-600'
-          }  px-3 py-2 text-sm text-white`}
-          disabled={useMutateUploadPostImg.isLoading || !editedPost.title}
-        >
+      <div className='mb-8 flex justify-center'>
+        <Button type='submit' disabled={useMutateUploadPostImg.isLoading || !editedPost.title}>
           {editedPost.id ? 'Update' : 'Create'}
-        </button>
+        </Button>
       </div>
       <div className='flex justify-center'>{useMutateUploadPostImg.isLoading && <Spinner />}</div>
     </form>
